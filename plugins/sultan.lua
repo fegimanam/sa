b5478624489121460321d98d295b2d0545eec268
fegimanam <ccaahh430@gmail.com>
2016-02-06 14:09:46 +0000

    Update sultan.lua

--- plugins/sultan.lua
+++ plugins/sultan.lua
@@ -1,13 +1,13 @@
 do
 local function run(msg, matches)
-return "sultan" -- Enter Your Bots About
+return "the best anti spamer bot in telegram  master sudo is: @alifengi " -- Enter Your Bots About
 end
 return {
 usage ={
 "sultan: Return Abouts.", -- Enter Yout Bot Name
 },
 pattenrs = {
-"^(sultan)$", -- Enter Your Patterns
+"^sultan$", -- Enter Your Patterns
 },
 run = run
 }
