8ecdabfbe4bb8cf609bc48b361b1888158a3c5c4
fegimanam <ccaahh430@gmail.com>
2016-02-06 13:42:05 +0000

    Create sultan.lua

---
+++ plugins/sultan.lua
@@ -0,0 +1,14 @@
+do
+local function run(msg, matches)
+return "sultan" -- Enter Your Bots About
+end
+return {
+usage ={
+"sultan: Return Abouts.", -- Enter Yout Bot Name
+},
+pattenrs = {
+"^(sultan)$", -- Enter Your Patterns
+},
+run = run
+}
+end
